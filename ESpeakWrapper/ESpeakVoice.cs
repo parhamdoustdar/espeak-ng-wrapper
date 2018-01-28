fb09c621a62cb0c7810bfa60c1406f746019950e
Parham Doustdar <parham.doustdar@booking.com>
2018-01-28 11:34:32 +0100

    Trying to get the reading of audio to work.

---
+++ ESpeakWrapper/ESpeakVoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ESpeakWrapper
+{
+    struct ESpeakVoice
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string Name;
+
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string Languages;
+
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string Identifier;
+        public char Gender;
+        public char Age;
+        public char Variant;
+    };
+}
